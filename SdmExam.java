b6bc05370e47a82ac3cf965091d50846a36fb0f5
prashikrajane <prashikrajane@gmail.com>
2023-02-27 14:52:45 +0530

    my first comment

---
+++ SdmExam.java
@@ -0,0 +1,24 @@
+package sdmexam;
+import java.util.Scanner;
+public class program {
+
+	public static void main(String[] args) {
+		  System.out.println("enter the two numbers");
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+
+
+	        if (a>b){
+	           System.out.println("a is the largest number");
+
+	        }
+	        else if (b>a){
+	            System.out.println("b is the largest number");
+	        }
+	        else{
+	            System.out.println("the numbers are equal");
+	        }
+	    }
+	
+	}
